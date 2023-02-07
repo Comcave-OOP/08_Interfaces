49f367c0d526aab5278095180f3be3208400e873
Marinus <marinus.fehling@web.de>
2023-02-07 10:36:00 +0100

    Interfaces, packages

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,5 +1,8 @@
 package com.cc.java;
 
+import com.cc.java.bees.*;
+import com.cc.java.birds.Bird;
+
 public class App {
     
     public static void main(String[] args) {
@@ -14,17 +17,17 @@ public class App {
         pollObj(drone);
         pollObj(bird);
 
-        // HoneyBee ist abstrakt --> Fehler!
-        // HoneyBee bee = new HoneyBee();
-        // output(bee.doYourJob());
-
-
     }
 
-    // 2. Variante Polymorphie
+    // Polymorphie + Overloading
     private static void pollObj(HoneyBee bee) {
         output(bee.doYourJob());
         output(bee.fly());
+    }
+
+    private static void pollObj(Bird bird){
+        output(bird.fly());
+        output(bird.hasFeathers());
     }
 
    
